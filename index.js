f8679bbba5ede6551671b71f12c7b9b4eb030487
Jae Kwon <136537986+Buyble-NYC@users.noreply.github.com>
2024-01-30 13:02:05 +0900

    q

--- index.js
+++ index.js
@@ -1,33 +1,31 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const cors = require("cors");
-//it was something like interpreting html 
+require("dotenv").config();
+//it was something like interpreting html
 
 app.use(express.json());
 app.use(cors());
 
-const db = require('./models')
+const db = require("./models");
 
 //Routers
-const postRouter = require('./routes/Posts')
+const postRouter = require("./routes/Posts");
 app.use("/posts", postRouter);
-const commentRouter = require('./routes/Comments')
+const commentRouter = require("./routes/Comments");
 app.use("/comments", commentRouter);
-const userRouter = require('./routes/Users')
+const userRouter = require("./routes/Users");
 app.use("/auth", userRouter);
-const likeRouter = require('./routes/Likes')
+const likeRouter = require("./routes/Likes");
 app.use("/like", likeRouter);
 
-
-db.sequelize.sync().then(()=>{
-    app.listen(process.env.PORT || 3001, ()=>{
-        console.log("Server running on port 3001");
-    })
-    // .catch((err)=>{
-    //     console.log("erroreee: ", err);
-    // }) DOESNT WORK FOR LISTEN
-})
-
-
-
-
+db.sequelize
+  .sync()
+  .then(() => {
+    app.listen(process.env.PORT || 3001, () => {
+      console.log("Server running on port 3001");
+    });
+  })
+  .catch((err) => {
+    console.log("erroreee: ", err);
+  });
